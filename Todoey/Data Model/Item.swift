266803ab07cba4cc346389958757f5b2583a1dcc
Der3k <der3kburrola@gmail.com>
2019-05-31 12:20:15 -0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Derek Burrola on 5/31/19.
+//  Copyright © 2019 Derek Burrola. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title:String = ""
+    var done:Bool = false
+    
+}
